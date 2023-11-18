ef6c43b34319d997d15080c688ae3ef856c72584
Tăng Tường Thoại <thoaitang24603@gmail.com>
2023-11-19 00:13:23 +0700

    update

--- chat-server/src/test/java/com/hcmus/chatserver/ChatServerApplicationTests.java
+++ chat-server/src/test/java/com/hcmus/chatserver/ChatServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.hcmus.chatserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @SpringBootTest
+@WebAppConfiguration
 class ChatServerApplicationTests {
 
     @Test
