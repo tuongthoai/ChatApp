dc79dc26c2abf329b9a76fe8183bf7623d66c8f0
QuanLac <89645977+LacTQuan@users.noreply.github.com>
2023-12-02 09:30:17 +0700

    admin table

---
+++ chatclientadmin/src/main/java/com/hcmus/ui/layout/Layout.java
@@ -0,0 +1,12 @@
+package com.hcmus.ui.layout;
+
+import javax.swing.*;
+
+public class Layout extends JFrame {
+    public Layout() {
+        setTitle("Layout");
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
---
+++ chatclientadmin/src/main/java/com/hcmus/ui/table/Table.java
@@ -0,0 +1,130 @@
+package com.hcmus.ui.table;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import java.awt.*;
+import java.lang.reflect.Field;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+import java.util.function.Predicate;
+
+public class Table<T> extends JScrollPane {
+    private final List<String> columnNames;
+    private List<T> data;
+    private final JTable table;
+
+    private final DefaultTableModel model;
+
+    public Table(List<T> data, List<String> columnNames) throws SQLException {
+        this.data = data;
+        this.columnNames = columnNames;
+        model = new DefaultTableModel(new Vector<>(columnNames), 0);
+        table = new JTable(model);
+
+        TableColumn column = null;
+        for (int i = 0; i < 5; i++) {
+            column = table.getColumnModel().getColumn(i);
+            if (i == 0) {
+                // id
+                column.setPreferredWidth(50);
+            } else if (i == 1 || i == 2) {
+                // first name, last name
+                column.setPreferredWidth(100);
+            } else if (i == 3) {
+                // dob
+                column.setPreferredWidth(150);
+            } else {
+                // address
+                column.setPreferredWidth(300);
+            }
+        }
+
+        table.setFillsViewportHeight(true);
+        setViewportView(table);
+        updateTable();
+    }
+
+    public void updateTable() {
+        model.setRowCount(0);
+
+        for (T item : data) {
+            Vector<Object> row = new Vector<>();
+            for (String columnName : columnNames) {
+                Field field = getFieldByName(item.getClass(), columnName.toLowerCase().replaceAll("\\s", ""));
+                if (field != null) {
+                    try {
+                        field.setAccessible(true);
+                        Object value = field.get(item);
+                        if (value instanceof LocalDate) {
+                            value = ((LocalDate) value).toString();
+                        }
+                        row.add(value);
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                } else {
+                    System.out.println("Field " + columnName + " not found");
+                    row.add(null);
+                }
+            }
+            model.addRow(row);
+        }
+
+    }
+
+    private Field getFieldByName(Class<?> clazz, String fieldName) {
+        try {
+            return clazz.getDeclaredField(fieldName);
+        } catch (NoSuchFieldException e) {
+            return null;
+        }
+    }
+
+    private boolean isNumeric(String str) {
+        return str.matches("\\d+");
+    }
+
+    private boolean ignoreCaseContains(String src, String keyword) {
+        return src.toLowerCase().contains(keyword.toLowerCase());
+    }
+
+    public void updateData(List<T> data) {
+        this.data = data;
+    }
+
+//    public void filterTable(String keyword) {
+//        Predicate<Student> matchesKeyword = student ->
+//                keyword.isEmpty() ||
+//                        isNumeric(keyword) && student.getId() == Integer.parseInt(keyword) ||
+//                        ignoreCaseContains(student.getFirstName(), keyword) ||
+//                        ignoreCaseContains(student.getLastName(), keyword) ||
+//                        ignoreCaseContains(student.getAddress(), keyword) ||
+//                        ignoreCaseContains(student.getDob().toString(), keyword);
+//        ArrayList<Student> filteredStudents = new ArrayList<>(students.stream()
+//                .filter(matchesKeyword)
+//                .toList());
+//
+//        model.setRowCount(0);
+//        filteredStudents.forEach(student -> {
+//            model.addRow(new Object[]{
+//                    student.getId(),
+//                    student.getFirstName(),
+//                    student.getLastName(),
+//                    student.getDob(),
+//                    student.getAddress()
+//            });
+//        });
+//    }
+
+
+
+    public JTable getTable() {
+        return table;
+    }
+}
