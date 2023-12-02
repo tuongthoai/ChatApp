ffd782e4e786aa1462f9b0a7916de6ceb1ed2980
QuanLac <89645977+LacTQuan@users.noreply.github.com>
2023-12-02 13:57:49 +0700

    data test for table

--- chatclientadmin/src/main/java/com/hcmus/Main.java
+++ chatclientadmin/src/main/java/com/hcmus/Main.java
@@ -1,7 +1,19 @@
 package com.hcmus;
 
+import com.hcmus.ui.layout.Layout;
+import javax.swing.*;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SwingUtilities.invokeLater(() -> {
+            try {
+                new Layout();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
+
+
 }
