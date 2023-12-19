4ba3096e28382852502ca400edfd3d81d8da885b
Khanh Nguyen <Kn11072003a@gmail.com>
2023-12-19 21:31:59 +0700

    Update UserActivity.java

--- chatclientadmin/src/main/java/com/hcmus/ui/screens/useractivity/UserActivity.java
+++ chatclientadmin/src/main/java/com/hcmus/ui/screens/useractivity/UserActivity.java
@@ -1,4 +1,51 @@
 package com.hcmus.ui.screens.useractivity;
 
-public class UserActivity {
+import com.hcmus.entities.user.User;
+import com.hcmus.entities.user.UserActivity;
+import com.hcmus.services.UserService;
+import com.hcmus.ui.table.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
+public class UserActivity extends JPanel {
+    private Table<UserActivity> table;
+    private SearchBar searchBar;
+    private UserService service;
+
+    private DateRangeSelector dateRangeSelector;
+
+    public UserActivity() {
+        setLayout(new BorderLayout());
+        try {
+            service = new UserService();
+            java.util.List<UserActivity> data = service.getAllUserActivity();
+            java.util.List<String> columnNames = UserActivity.getColumnNames();
+
+            table = new Table<>(data, columnNames);
+            searchBar = new SearchBar(table.getSorter());
+            dateRangeSelector = new DateRangeSelector();
+
+            // add event listener to date range selector
+            dateRangeSelector.getSearchButton().addActionListener(e -> {
+                try {
+                    java.util.List<UserActivity> filteredData = service.getUserActivity(dateRangeSelector.getStartDate(), dateRangeSelector.getEndDate());
+                    System.out.println(filteredData.size());
+                    table.updateData(filteredData);
+                    table.updateTable();
+                } catch (Exception ex) {
+                    ex.printStackTrace(System.err);
+                }
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        }
+
+        setLayout(new BorderLayout());
+        add(searchBar, BorderLayout.NORTH);
+        add(table, BorderLayout.CENTER);
+        add(dateRangeSelector, BorderLayout.SOUTH);
+    }
 }
