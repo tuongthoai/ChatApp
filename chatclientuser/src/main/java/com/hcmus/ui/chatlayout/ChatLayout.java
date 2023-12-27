0f514b71628988b44e0d367ac64e7f3ed8bb0cb5
Tăng Tường Thoại <thoaitang24603@gmail.com>
2023-12-27 23:26:43 +0700

    fix not exit when close

--- chatclientuser/src/main/java/com/hcmus/ui/chatlayout/ChatLayout.java
+++ chatclientuser/src/main/java/com/hcmus/ui/chatlayout/ChatLayout.java
@@ -24,6 +24,7 @@ public class ChatLayout extends JFrame {
 
     public ChatLayout() throws URISyntaxException, SQLException {
         setTitle("Chat UI");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 //        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         addWindowListener(new WindowAdapter() {
             @Override
